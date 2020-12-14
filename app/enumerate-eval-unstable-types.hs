170973ddae1f0c8acf9fd5e706ce9a1b10c85761
Scott J Maddox <scottjmaddox@gmail.com>
2020-12-13 21:44:11 -0600

    add enumerate-eval-unstable-types.hs

---
+++ app/enumerate-eval-unstable-types.hs
@@ -0,0 +1,32 @@
+-- Copyright (c) 2020 Scott J Maddox
+--
+-- Licensed under either the Apache License, Version 2.0 (see LICENSE-APACHE),
+-- or the ZLib license (see LICENSE-ZLIB), at your option.
+
+module Main where
+
+import Control.Monad
+import Data.Either
+import Language.Dawn.Phase1.Core
+import Language.Dawn.Phase1.Display
+import Language.Dawn.Phase1.Exprs
+import Language.Dawn.Phase1.Utils
+
+main = do
+  let iter e = do
+        let t = inferNormType e
+        let e' = partialEval e
+        let t' = inferNormType e'
+        when
+          ((isRight t || isRight t') && t /= t')
+          ( do
+              printExprType e t
+              printExprType e' t'
+              putStrLn ""
+          )
+  mapM iter (allExprsUpToWidthAndDepth 4 1)
+
+printExprType e t = do
+  case t of
+    Left err -> putStrLn $ display e ++ " is not typeable. " ++ display err
+    Right t' -> putStrLn $ display e ++ " :: " ++ display t'
