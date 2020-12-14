ca361b7e6fa501d735fd6a06e93964e05979114e
Scott J Maddox <scottjmaddox@gmail.com>
2020-12-13 21:08:02 -0600

    add enumerate-grouping-unstable-types.hs

---
+++ app/enumerate-grouping-unstable-types.hs
@@ -0,0 +1,32 @@
+-- Copyright (c) 2020 Scott J Maddox
+--
+-- Licensed under either the Apache License, Version 2.0 (see LICENSE-APACHE),
+-- or the ZLib license (see LICENSE-ZLIB), at your option.
+
+module Main where
+
+import Control.Monad
+import Data.Either
+import Language.Dawn.Phase1.Core
+import Language.Dawn.Phase1.Display
+import Language.Dawn.Phase1.Exprs
+import Language.Dawn.Phase1.Utils
+
+main = do
+  mapM printExprType (allExprsUpToWidthAndDepth 4 1)
+
+printExprType e = do
+  let t = inferNormType e
+  let es = allGroupings e
+  let ts = filter isRight (map inferNormType es)
+  when
+    (length ts > 1 && any (/= t) ts)
+    ( do
+        mapM_
+          ( \(e', t') -> case t' of
+              Left err -> putStrLn $ display e' ++ " is not typeable. " ++ display err
+              Right t'' -> putStrLn $ display e' ++ " :: " ++ display t''
+          )
+          (zip es ts)
+        putStrLn ""
+    )
