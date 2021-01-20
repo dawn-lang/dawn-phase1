49b679804c15b669c66afa4c4e2d8dbb415f66f7
Scott J Maddox <scottjmaddox@gmail.com>
2021-01-19 22:54:42 -0600

    dawni: switch to eval

--- app/dawni.hs
+++ app/dawni.hs
@@ -5,9 +5,15 @@
 
 module Main where
 
+import Control.Exception
 import Control.Monad
+import Control.Monad.IO.Class
+import Data.List
+import qualified Data.Map as Map
+import Data.Maybe
 import Language.Dawn.Phase1.Core
 import Language.Dawn.Phase1.Display
+import Language.Dawn.Phase1.Eval
 import Language.Dawn.Phase1.Parse
 import Language.Dawn.Phase1.PartialEval
 import Language.Dawn.Phase1.Utils
@@ -16,22 +22,45 @@ import System.IO
 
 main = do
   putStrLn "Dawn Phase 1 Interpreter"
-  runInputT defaultSettings readEvalPrintLoop
+  runInputT defaultSettings (readEvalPrintLoop (MultiStack Map.empty))
 
-readEvalPrintLoop = readEvalPrint >> readEvalPrintLoop
+readEvalPrintLoop :: MultiStack -> InputT IO MultiStack
+readEvalPrintLoop ms = do
+  ms' <- readEvalPrint ms
+  readEvalPrintLoop ms'
 
-readEvalPrint = do
+readEvalPrint :: MultiStack -> InputT IO MultiStack
+readEvalPrint ms = do
   maybeLine <- getInputLine "> "
   case maybeLine of
-    Nothing -> return ()
-    Just line -> case parseExpr line of
-      Left err -> outputStrLn (show err)
-      Right e -> do
-        printExprType e
-        when (e /= partialEval' e) $ printExprType (partialEval' e)
+    Nothing -> return ms
+    Just line | ":print " `isPrefixOf` line -> do
+      case parseExpr (fromJust $ stripPrefix ":print " line) of
+        Left err -> outputStrLn (show err)
+        Right e ->
+          case inferNormType' e of
+            Left err -> outputStrLn $ display e ++ " is not typeable. " ++ display err
+            Right t -> outputStrLn $ display e ++ " :: " ++ display t
+      return ms
+    Just line ->
+      case parseExpr line of
+        Left err -> do
+          outputStrLn (show err)
+          return ms
+        Right e ->
+          case inferNormType' e of
+            Left err -> do
+              outputStrLn $ display e ++ " is not typeable. " ++ display err
+              return ms
+            Right t -> do
+              result <- try' (evaluate (eval ["$"] e ms))
+              case (result :: Either SomeException MultiStack) of
+                Left err -> do
+                  outputStrLn $ show err
+                  return ms
+                Right ms' -> do
+                  outputStrLn $ display ms'
+                  return ms'
 
-printExprType e = do
-  outputStr (display e)
-  case inferNormType' e of
-    Left e -> outputStrLn $ " is not typeable. " ++ display e
-    Right t -> outputStrLn $ " :: " ++ display t
+try' :: Exception e => IO a -> InputT IO (Either e a)
+try' = liftIO . Control.Exception.try
